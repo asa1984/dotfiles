2c504095ebf73333a28d6de0ab6e170b23a5e87a
ASA1984 <satoasa9913@gmail.com>
2023-01-12 16:57:37 +0900

    add: dev tools

--- modules/tools/default.nix
+++ modules/tools/default.nix
@@ -3,11 +3,14 @@
     bat # cat alternative
     bottom # top alternative
     exa # ls alternative
+    du-dust # du alternative
     fd # find alternative
+    httpie # http client
     jq # json parser
     lazydocker # docker tui
     lazygit # git tui
-    neofetch
+    neofetch # system info
+    procs # ps alternative
     ranger # file viewer
     ripgrep # grep alternative
     tokei
