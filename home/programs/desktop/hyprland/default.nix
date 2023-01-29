b208ee03e11732f1ebab614d3e78bba4e8ed2e1a
ASA1984 <satoasa9913@gmail.com>
2023-01-30 01:48:05 +0900

    add: wofi-emoji

--- home/programs/desktop/hyprland/default.nix
+++ home/programs/desktop/hyprland/default.nix
@@ -14,9 +14,11 @@
   home.packages = with pkgs; [
     wofi
     wofi-emoji
+    wtype
   ];
   wayland.windowManager.hyprland = {
     enable = true;
+    disableAutoreload = true;
     extraConfig = import ./config.nix {inherit colorscheme;};
   };
 }
