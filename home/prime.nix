83122da9c4c3269a3d3ee579ade5d0061c1112d2
ASA1984 <satoasa9913@gmail.com>
2023-02-21 21:35:09 +0900

    refactor: directory structure

--- home/prime.nix
+++ home/prime.nix
@@ -4,6 +4,7 @@
     ./home.nix
 
     ./programs/cli
+    ./programs/gui
     ./programs/desktop/hyprland
   ];
 }
--- home/programs/desktop/hyprland/default.nix
+++ home/programs/desktop/hyprland/default.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   imports = [
-    ../common
     ./eww
     ./mako.nix
   ];
