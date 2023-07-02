e1c579858e30ada0adf91a89e0eb3ad774a50b8b
asa1984 <satoasa9913@gmail.com>
2023-07-02 15:33:42 +0900

    rhine: update hardware and boot configurations

--- hosts/rhine/nixos.nix
+++ hosts/rhine/nixos.nix
@@ -12,9 +12,8 @@
     ../../modules/virtualisation.nix
   ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = hostname;
 
