f6ddfde4cc06c18de97e0d2dd252a7ada31af0c9
asa1984 <satoasa9913@gmail.com>
2023-07-11 23:03:04 +0900

    ranger: enable image preview on wezterm

--- home-manager/gui/terminals/wezterm/default.nix
+++ home-manager/gui/terminals/wezterm/default.nix
@@ -5,5 +5,11 @@
   };
   home.file = {
     ".config/wezterm/colors/myTheme.toml".text = theme.wezterm;
+
+    # ranger config - enable image preview on wezterm
+    ".config/ranger/rc.conf".text = ''
+      set preview_images true
+      set preview_images_method iterm2
+    '';
   };
 }
