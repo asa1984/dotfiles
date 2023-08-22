8408c7dfa1da1badf4efa89b2ae8a2bba1133f1b
asa1984 <satoasa9913@gmail.com>
2023-08-23 02:46:18 +0900

    neovim: fix rustfmt

--- home-manager/cli/neovim/config/lua/plugins/language.lua
+++ home-manager/cli/neovim/config/lua/plugins/language.lua
@@ -20,7 +20,23 @@ null_ls.setup({
 		-- Python
 		null_ls.builtins.formatting.black,
 		-- Rust
-		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.formatting.rustfmt.with({
+			extra_args = function(params)
+				local Path = require("plenary.path")
+				local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+				if cargo_toml:exists() and cargo_toml:is_file() then
+					for _, line in ipairs(cargo_toml:readlines()) do
+						local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+						if edition then
+							return { "--edition=" .. edition }
+						end
+					end
+				end
+				-- default edition when we don't find `Cargo.toml` or the `edition` in it.
+				return { "--edition=2021" }
+			end,
+		}),
 		-- Shell
 		null_ls.builtins.diagnostics.shellcheck,
 		null_ls.builtins.formatting.shfmt,
