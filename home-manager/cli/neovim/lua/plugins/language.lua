0bfea035bb31baf4e457dddecc6c4cddee386f62
asa1984 <satoasa9913@gmail.com>
2023-12-18 13:12:31 +0900

    neovim: add conform.nvim for typstfmt

--- home-manager/cli/neovim/lua/plugins/language.lua
+++ home-manager/cli/neovim/lua/plugins/language.lua
@@ -132,4 +132,42 @@ return {
 		ft = "typst",
 		lazy = false,
 	},
+
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		-- Everything in opts will be passed to setup()
+		opts = {
+			-- -- Define your formatters
+			formatters_by_ft = {
+				typst = { "typstfmt" },
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+
+			formatters = {
+				typstfmt = {
+					command = "typstfmt",
+					args = { "$FILENAME" },
+					stdin = false,
+				},
+			},
+		},
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
 }
