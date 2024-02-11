227d081375718f06d0a609d7fa858224830746fb
asa1984 <satoasa9913@gmail.com>
2024-02-11 23:28:37 +0900

    add video player & pdf viewer

--- home-manager/gui/media.nix
+++ home-manager/gui/media.nix
@@ -1,15 +1,11 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [spotify];
+  home.packages = with pkgs; [
+    spotify
+    gnome.totem # video player
+    gnome.evince # pdf viewer
+  ];
 
-  # Spotify TUI
-  programs.ncspot = {
-    enable = true;
-  };
-
-  services.easyeffects = {
-    enable = true;
-  };
-  programs.obs-studio = {
-    enable = true;
-  };
+  programs.ncspot.enable = true; # spotify tui
+  services.easyeffects.enable = true;
+  programs.obs-studio.enable = true;
 }
