eea05a9008d946e4f5e6d7a38cf0ae94105c14dc
asa1984 <satoasa9913@gmail.com>
2024-02-17 15:25:03 +0900

    fix colortheme module

--- themes/default.nix
+++ themes/default.nix
@@ -1,11 +1,10 @@
 theme: let
   removeHash = str: builtins.substring 1 (builtins.stringLength str) str;
-in rec
-{
+in rec {
   # "#RRGGBB"
   xcolors = import ./colors/${theme};
   # "RRGGBB"
-  colors = builtins.mapAttrs (_: value: removeHash value) colors;
+  colors = builtins.mapAttrs (_: value: removeHash value) xcolors;
   # wezterm colors
   wezterm = builtins.readFile ./colors/${theme}/wezterm.toml;
   # fzf colors
