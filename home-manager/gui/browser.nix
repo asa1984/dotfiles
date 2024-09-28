9bb5e9c50946ead9d6ea13f442d996cf8391b940
asa1984 <satoasa9913@gmail.com>
2024-09-28 10:48:30 +0900

    feat: enable widevine of vivaldi

--- home-manager/gui/browser.nix
+++ home-manager/gui/browser.nix
@@ -1,9 +1,14 @@
+{ pkgs, ... }:
 {
   programs = {
     firefox.enable = true;
     google-chrome.enable = true;
     vivaldi = {
       enable = true;
+      package = pkgs.vivaldi.overrideAttrs {
+        proprietaryCodecs = true;
+        enableWidevine = true;
+      };
       commandLineArgs = [
         "--enable-features=WebUIDarkMode"
         "--force-dark-mode"
