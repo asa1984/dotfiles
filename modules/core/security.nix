8b7569241603e01c1894263355ce88cd770d637b
asa1984 <satoasa9913@gmail.com>
2024-10-05 17:11:06 +0900

    feat(git): enable gpg signing

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,4 +1,10 @@
+{ pkgs, ... }:
 {
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+  };
+
   security = {
     # allow members of the wheel group to run sudo without a password
     sudo.wheelNeedsPassword = false;
