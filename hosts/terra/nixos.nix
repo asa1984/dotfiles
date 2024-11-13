d88faf8726c82138db04f84f7982b1452269a39a
asa1984 <satoasa9913@gmail.com>
2024-11-13 23:37:43 +0900

    feat: add sunshine

--- hosts/terra/nixos.nix
+++ hosts/terra/nixos.nix
@@ -62,4 +62,11 @@
       };
     };
   };
+
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
+    openFirewall = true;
+  };
 }
