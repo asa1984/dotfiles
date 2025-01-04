c4ebcc9d6df6076945baad6d7780455d6dd16970
asa1984 <satoasa9913@gmail.com>
2025-01-05 05:00:50 +0900

    refactor: split nix-darwin configs

---
+++ configs/nix-darwin/macos-defaults.nix
@@ -0,0 +1,36 @@
+{
+  system = {
+    defaults = {
+      CustomUserPreferences."com.apple.AppleMultitouchTrackpad".DragLock = true;
+      dock = {
+        autohide = true;
+        largesize = 64;
+        magnification = true;
+        mineffect = "scale";
+        show-recents = false;
+        tilesize = 50;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "clmv";
+        ShowPathbar = true;
+        ShowStatusBar = true;
+      };
+      NSGlobalDomain = {
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+      };
+      trackpad = {
+        Clicking = true;
+        Dragging = true;
+      };
+    };
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+  };
+}
