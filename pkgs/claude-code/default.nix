c27993f081517e546e7cddbf1fc21d670133c622
asa1984 <satoasa9913@gmail.com>
2025-07-12 10:54:26 +0900

    claude-code: 1.0.27 -> 1.0.51

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -1,9 +1,9 @@
 { claude-code, fetchzip }:
 claude-code.overrideAttrs (oldAttrs: rec {
-  version = "1.0.27";
+  version = "1.0.51";
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-HnlJo93RfsZdUZiXfnojDjp3BVSBNJ3YX/6silX9VUU=";
+    hash = "sha256-sAILRsi8ZViMfcpqykfnFQzHTJHRwRSZz45otMqa4U0=";
   };
   npmDepsHash = "sha256-7jbvsIXulG6dJOnMc5MjDm/Kcyndm4jDSdj85eSp/hc=";
 })
