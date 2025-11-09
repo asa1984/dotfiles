3e76f2985edc7c477694006660d9fbe8eca301fb
asa1984 <satoasa9913@gmail.com>
2025-11-09 13:34:24 +0900

    feat: make git config better

--- configs/home-manager/git/default.nix
+++ configs/home-manager/git/default.nix
@@ -1,26 +1,50 @@
 {
   programs.git = {
     enable = true;
-    userName = "asa1984";
-    userEmail = "satoasa9913@gmail.com";
     signing = {
       key = "D6B5DF95C5360AE9";
       signByDefault = false;
     };
-    extraConfig = {
+    settings = {
+      user = {
+        email = "satoasa9913@gmail.com";
+        name = "asa1984";
+      };
+
       init.defaultBranch = "main";
+
       commit.gpgsign = false;
-      merge.conflictStyle = "diff3";
-      rebase.autoSquash = true;
-      pull.rebase = true;
+      fetch = {
+        prune = true;
+        pruneTags = true;
+      };
+      merge.conflictStyle = "zdiff3";
+      pull = {
+        autoStash = true;
+        rebase = true;
+      };
       push.autoSetupRemote = true;
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
     };
     ignores = [
-      ".direnv"
-      ".devenv"
-      ".neoconf.json"
+      ".DS_Store"
       ".claude/settings.local.json"
+      ".devenv"
+      ".direnv"
+      ".neoconf.json"
     ];
-    difftastic.enable = true;
+  };
+
+  programs.difftastic = {
+    enable = true;
+    git.enable = true;
   };
 }
